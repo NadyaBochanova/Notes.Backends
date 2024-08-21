cd5755f2c42489f6f50d26de0bd815db0356e4be
nBochanova <nadyabochanova@gmail.com>
2024-08-21 17:21:07 +0300

    add NoteController

---
+++ Notes.WebApi/Controllers/NoteController.cs
@@ -0,0 +1,70 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Notes.Application.Notes.Commands.CreateNote;
+using Notes.Application.Notes.Commands.DeleteNote;
+using Notes.Application.Notes.Commands.UpdateNote;
+using Notes.Application.Notes.Queries.GetNoteDetails;
+using Notes.Application.Notes.Queries.GetNoteList;
+using Notes.WebApi.Models;
+
+namespace Notes.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class NoteController : BaseController
+    {
+        private readonly IMapper _mapper;
+
+        public NoteController(IMapper mapper) => _mapper = mapper;
+
+        [HttpGet]
+        public async Task<ActionResult<NoteListVm>> GetAll()
+        {
+            var query = new GetNoteListQuery
+            {
+                UserId = UserId
+            };
+            var vm = await Mediator.Send(query);
+            return Ok(vm);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<NoteDetailsVm>> Get(Guid id)
+        {
+            var query = new GetNoteDetailsQuery
+            {
+                UserId = UserId,
+                Id = id
+            };
+            var vm = await Mediator.Send(query);
+            return Ok(vm);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Guid>> Create([FromBody] CreateNoteDto createNoteDto)
+        {
+            var cmd = _mapper.Map<CreateNoteCommand>(createNoteDto);
+            cmd.UserId = UserId;
+
+            var noteId = await Mediator.Send(cmd);
+            return Ok(noteId);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update([FromBody] UpdateNoteDto updateNoteDto)
+        {
+            var cmd = _mapper.Map<UpdateNoteCommand>(updateNoteDto);
+            cmd.UserId = UserId;
+
+            await Mediator.Send(cmd);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete([FromBody] Guid guid)
+        {
+            var cmd = new DeleteNoteCommand { Id = guid, UserId = UserId};
+            await Mediator.Send(cmd);
+            return NoContent();
+        }
+    }
+}
